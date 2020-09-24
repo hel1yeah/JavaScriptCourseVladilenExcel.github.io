26f1ee000e8d4a85dee0b85c8d41f7c6ac083bf8
hel1yeah <hel1_yeah@ukr.net>
2020-09-24 23:06:34 +0300

    basic settings the end
    1. add plugins
    1.clean-webpack-plugin,
    2 . copy-webpack-plugin,
    3. html-webpack-plugin,
    4 mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,39 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
   entry: './index.js',
   output : {
-    filename: 'bundle.js',
+    filename: 'bundle.[hash].js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  resolve:{
+    extensions: ['.js'],
+    alias:{
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src/code'),
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(), // плагин для очистки билда
+    new HtmlWebpackPlugin({
+      template: 'index.html' // вобавляет сюда каждый билд новый создайнны bundle.blablabla.js
+    }),
+    new CopyPlugin([
+    {
+      from: path.resolve(__dirname, 'src/favicon.ico'),
+        to: path.resolve(__dirname, 'dist')
+    }
+    ]),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.[hash].css'
+    })
+  ]
+  
 
 };
