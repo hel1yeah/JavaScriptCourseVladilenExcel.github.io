86a093d7e73cbec7a739f4225f55b9405378cd38
hel1yeah <hel1_yeah@ukr.net>
2020-09-25 17:14:06 +0300

    15 lesson, settings loaders

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,32 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'bundle.[hash].css'
     })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ],
+  },
   
 
 };
