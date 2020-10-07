20ef42bcc7d7b5d66f3a1c1bd02ff4060abdf228
hel1yeah <hel1_yeah@ukr.net>
2020-10-07 12:58:08 +0300

    test save

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,14 +3,14 @@ const CODES = {
   Z: 90,
 };
 
-function toCell() {
+function toCell(_, index) {
   return `
-  <div class="cell" contenteditable></div>
+  <div class="cell" contenteditable data-col="${index}"></div>
   `;
 }
-function toColumn(colName) {
+function toColumn(colName, index) {
   return `
-  <div class="column" data-type="resizable">
+  <div class="column" data-type="resizable" data-col="${index}">
   ${colName}
   <div class="col-resize" data-resize="col"></div>
   </div>
@@ -20,7 +20,7 @@ function toColumn(colName) {
 function createRow(index, content) {
   const resizer = index ? '<div class="row-resize" data-resize="row"></div>' : '';
   return `
-    <div class="row">
+    <div class="row" data-type="resizable">
       <div class="row-info">${index}
       ${resizer}
       </div>
