c73edfe43f5eeca992bda9cae53bef9ab7abda50
hel1yeah <hel1_yeah@ukr.net>
2020-10-07 15:31:01 +0300

    finish resize table

--- src/components/table/TableComponent.js
+++ src/components/table/TableComponent.js
@@ -1,6 +1,8 @@
 import {ExecelComponent} from '@core/ExecelComponent';
 import {createTable} from './table.template';
-import {$} from '../../core/dom';
+import {resizeHandler} from './table.resize';
+import {shouldResize} from './table.function';
+// import {$} from '../../core/dom';
 
 export class TableComponent extends ExecelComponent {
   static className = 'exel__table';
@@ -16,23 +18,8 @@ export class TableComponent extends ExecelComponent {
   }
 
   onMousedown(event) {
-    // console.log(event.target.getAttribute('data-resize'));
-
-    if (event.target.dataset.resize) {
-      const $resizer = $(event.target);
-      // const $perent = $resizer.$el.parentNode;
-      // const $perent = $resizer.$el.closest('.column');
-      const $parent = $resizer.closest('[data-type="resizable"]');
-      const coords = $parent.getCoords();
-      console.log();
-      document.onmousemove = e => {
-        const delta = e.pageX - coords.right;
-        // console.log(delta);
-        $parent.$el.style.width = coords.width + delta + 'px';
-      };
-      document.onmouseup = () => {
-        document.onmousemove = null;
-      };
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event);
     }
   }
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -55,6 +55,13 @@ class Dom {
   findAll(selector) {
     return this.$el.querySelectorAll(selector);
   }
+  css(styles = {}) {
+    Object
+        .keys(styles)
+        .forEach(key => {
+          this.$el.style[key] = styles[key];
+        });
+  }
 }
 
 export function $(selector) {
